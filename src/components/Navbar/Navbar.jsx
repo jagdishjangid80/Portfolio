6664f58ec79a11e45e870d2160c1c5f4d3dec2f2
jagdishjangid80 <jagdishjangid8079@gmail.com>
2025-02-26 19:08:42 +0530

    update all

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -22,48 +22,48 @@ function Navbar() {
   };
 
   return (
-      <>
-        <nav className="nav-wrapper">
-          <div className="nav-content">
-            <p className="logo">Jagdish Jangid</p>
-            <ul className={`nav-links ${MenuOpen ? "active" : ""}`}>
-              <li>
-                <a href="#home" className="menu-item" onClick={(e) => handleScroll(e, "home")}>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#skills" className="menu-item" onClick={(e) => handleScroll(e, "skills")}>
-                  Skills
-                </a>
-              </li>
-              <li>
-                <a href="#work-experience" className="menu-item" onClick={(e) => handleScroll(e, "work-experience")}>
-                  Work Experience
-                </a>
-              </li>
-              <li>
-                <a href="#contact" className="menu-item" onClick={(e) => handleScroll(e, "contact")}>
-                  Contact Me
-                </a>
-              </li>
-            </ul>
-            <a href="./assets/images/Resume.pdf" download className="contact-btn">
-              Hire Me
-            </a>
-            <button
-                className={`menu-btn ${MenuOpen ? "rotated" : ""}`}
-                onClick={toggleMenu}
-            >
-              {MenuOpen ? (
-                  <XMarkIcon className="h-8 w-8 text-white" />
-              ) : (
-                  <Bars3Icon className="h-8 w-8 text-white" />
-              )}
-            </button>
-          </div>
-        </nav>
-      </>
+    <>
+      <nav className="nav-wrapper">
+        <div className="nav-content">
+          <p className="logo">Jagdish Jangid</p>
+          <ul className={`nav-links ${MenuOpen ? "active" : ""}`}>
+            <li>
+              <a href="#home" className="menu-item" onClick={(e) => handleScroll(e, "home")}>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#skills" className="menu-item" onClick={(e) => handleScroll(e, "skills")}>
+                Skills
+              </a>
+            </li>
+            <li>
+              <a href="#work-experience" className="menu-item" onClick={(e) => handleScroll(e, "work-experience")}>
+                Work Experience
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="menu-item" onClick={(e) => handleScroll(e, "contact")}>
+                Contact Me
+              </a>
+            </li>
+          </ul>
+          <a href="./assets/images/Resume.pdf" download className="contact-btn">
+            Hire Me
+          </a>
+          <button
+            className={`menu-btn ${MenuOpen ? "rotated" : ""}`}
+            onClick={toggleMenu}
+          >
+            {MenuOpen ? (
+              <XMarkIcon className="h-8 w-8 text-white" />
+            ) : (
+              <Bars3Icon className="h-8 w-8 text-white" />
+            )}
+          </button>
+        </div>
+      </nav>
+    </>
   );
 }
 
